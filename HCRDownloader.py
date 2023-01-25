c44ecb4a5ebc5448e1d8368eeb3d67c76d4e6adf
Tiflo62 <74988027+Tiflo62@users.noreply.github.com>
2023-01-25 12:48:34 +0100

    Update HCRDownloader.py
    Check if the url is valid

--- HCRDownloader.py
+++ HCRDownloader.py
@@ -3,13 +3,22 @@ import re
 import requests
 import urllib.request
 
+def check_url(url):
+    match = re.match(r"https://heritage\.canadiana\.ca/view/oocihm\.lac_reel", url)
+    return match
+
 #dossier de DL
 folder = input("Enter a foldername: ")
 if not os.path.exists(folder):
     os.makedirs(folder)
 print("Directory '% s' created!" % folder)
+
 #URL du reel
 url = input("Enter a URL (ex:https://heritage.canadiana.ca/view/oocihm.lac_reel_t10533/1): ")
+while not check_url(url):
+    print("URL does not follow the specified rule. Please try again.")
+    url = input("Enter a URL: ")
+
 response = requests.get(url)
 source = response.text
 
